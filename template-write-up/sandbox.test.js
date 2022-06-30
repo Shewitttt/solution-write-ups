30c78f4ef473259bd457f5e284b8392ffa99b113
Shewitttt <89597132+Shewitttt@users.noreply.github.com>
2022-06-30 18:51:11 +0200

    template-write-up/sandbox.test.js

--- template-write-up/sandbox.test.js
+++ template-write-up/sandbox.test.js
@@ -1,15 +1,29 @@
 'use strict';
 
 /*
-
+test inspired by the example given in class 'Quarter of the year'
+test for push-to-new-array exercise
 */
 
-for (const solution of []) {
-  describe(`${solution.name}: ___`, () => {
-    describe('_', () => {
-      it('_', () => {
-        expect(solution(_)).toEqual(_);
-      });
-    });
-  });
+function addArrays(arr1, arr2) {
+  if (arr1.length != arr2.length) {
+    throw new Error("You done goof'd");
+  }
+  let sum = [];
+  for (let i = 0; i < arr1.length; i++) {
+    sum.push(arr1[i] + arr2[i]);
+  }
+  return sum;
 }
+
+describe("Array summation test", () => {
+  it("It should return [11, 42, 480] when [12, 0, -20] and [-1, 42, 500] are passed", () => {
+    expect(addArrays([12, 0, -20], [-1, 42, 500])).toEqual([11, 42, 480]);
+  });
+  it("It should return ['JavaScript','is cool.'] when ['Java', 'is'] and ['Script', ' cool.'] are passed", () => {
+    expect(addArrays(["Java", "is"], ["Script", " cool."])).toEqual([
+      "JavaScript",
+      "is cool.",
+    ]);
+  });
+});
